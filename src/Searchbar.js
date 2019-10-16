6e5600a014a4dc732d192ae89bf95ed3732e80b7
Allan Sham <allansham@hotmail.com>
2019-10-16 23:43:39 +0100

    Add states for the 3 form elements and update scss

--- src/Searchbar.js
+++ src/Searchbar.js
@@ -1,23 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Searchbar() {
+  const [searchFonts, setSearchFonts] = useState("");
+  const [typeSomething, setTypeSomething] = useState("Hello World");
+  const [fontSize, setFontSize] = useState("20px");
+
   return (
     <div className="search-bar-container">
       <div className="search-bar">
         <div className="fa fa-search"></div>
-        <input
-          className="search-fonts"
-          type="text"
-          placeholder="Search fonts"
-        />
-        <input className="type-something" placeholder=" Type something" />
 
-        <select name="font-size">
-          {/* <option value="">--select font size--</option> */}
-          <option value="20px"> 20px</option>
-          <option value="24px"> 24px</option>
-          <option value="32px"> 32px</option>
-          <option value="40px"> 40px</option>
+        <input
+          type="text"
+          name="searchFonts"
+          value={searchFonts}
+          className="search-fonts"
+          placeholder="Search fonts"
+          onChange={event => setSearchFonts(event.target.value)}
+        />
+
+        <input
+          type="text"
+          name="typeSomething"
+          value={typeSomething}
+          className="type-something"
+          placeholder="Type something"
+          onChange={event => setTypeSomething(event.target.value)}
+        />
+
+        <select
+          value={fontSize}
+          name="fontSize"
+          onChange={event => setFontSize(event.target.value)}
+        >
+          <option value="20px">20px</option>
+          <option value="24px">24px</option>
+          <option value="32px">32px</option>
+          <option value="40px">40px</option>
         </select>
 
         <div className="background-chooser">
@@ -31,6 +50,10 @@ function Searchbar() {
 
         <div className="fa fa-repeat"></div>
       </div>
+
+      <h1>{searchFonts}</h1>
+      <h1>{typeSomething}</h1>
+      <h1>{fontSize}</h1>
     </div>
   );
 }
