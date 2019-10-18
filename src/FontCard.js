2ea6924ae3236377d84a99ac4babf09c869c0393
Allan Sham <allansham@hotmail.com>
2019-10-18 21:31:51 +0100

    Implement feature so text shows in correct font

--- src/FontCard.js
+++ src/FontCard.js
@@ -1,20 +1,34 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 
-function FontCard({ family, searchFonts, typeSomething, fontSize }) {
+function FontCard({ family, typeSomething, fontSize }) {
+  //Add plus signs between words in the name of the font, to be used to retrive the fonts using the url
+  const addPlusSigns = family => {
+    if (family.indexOf(" ") === -1) {
+      return family;
+    } else {
+      return family.split(" ").join("+");
+    }
+  };
+
   return (
     <div className="fontcard">
       <div className="font-title">
+        <Helmet>
+          <link
+            href={`https://fonts.googleapis.com/css?family=${addPlusSigns(
+              family
+            )}`}
+            rel="stylesheet"
+          ></link>
+        </Helmet>
         <h4>{family}</h4>
         <div className="fa fa-plus-circle"></div>
       </div>
       <p
         style={{
           fontSize: fontSize,
-          fontFace: {
-            fontFamily: "Oswald",
-            src:
-              "http://fonts.gstatic.com/s/oswald/v26/TK3_WkUHHAIjg75cFRf3bXL8LICs1_FvgUFoZAaRliE.ttf"
-          }
+          fontFamily: family
         }}
       >
         {typeSomething}
