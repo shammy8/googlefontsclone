800555aaef47da679834d0ef1a57edd6cb305140
Allan Sham <allansham@hotmail.com>
2019-10-25 14:43:47 +0100

    Updates

---
+++ precache-manifest.1c6e83c7feb0572f68324a7359a239c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "231cebc2eda0236179faf6afc842e26c",
+    "url": "/googlefontsclone/index.html"
+  },
+  {
+    "revision": "60925bf2023d94956119",
+    "url": "/googlefontsclone/static/css/main.ec730a01.chunk.css"
+  },
+  {
+    "revision": "70e16bccd5d555d29fde",
+    "url": "/googlefontsclone/static/js/2.e08c809f.chunk.js"
+  },
+  {
+    "revision": "60925bf2023d94956119",
+    "url": "/googlefontsclone/static/js/main.2142be64.chunk.js"
+  },
+  {
+    "revision": "246a60d9ba8df14990b0",
+    "url": "/googlefontsclone/static/js/runtime-main.1a092555.js"
+  }
+]);
